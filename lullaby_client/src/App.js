cd2bb3692500c80d47ddae1d38d0a797de1e64a2
dfrymire79 <59580606+dfrymire79@users.noreply.github.com>
2020-07-10 12:11:07 -0500

    Displayed data on page, re-seeded database

--- lullaby_client/src/App.js
+++ lullaby_client/src/App.js
@@ -1,11 +1,75 @@
 import React, {Component} from 'react';
 import Lullabies from './components/Lullabies'
+import './App.css';
 
 class App extends Component {
+
+  state = {
+    lullabies: [],
+    formInputs: {
+      title: '',
+      artist: '',
+      lyrics: '',
+      image: '',
+      vid_link: ''
+      }
+    }
+  
+
+  handleChange = (event) => {
+    const updateInput = Object.assign( this.state.formInputs, { [event.target.id]: event.target.value })
+    this.setState(updateInput)
+  }
+  
+  handleSubmit = (event) =>{
+    event.preventDefault()
+    fetch('http://localhost:3000/lullabies', {
+      body: JSON.stringify(this.state.formInputs),
+      method: 'POST',
+   headers: {
+     'Accept': 'application/json, text/plain, */*',
+     'Content-Type': 'application/json'
+   }
+  })
+   .then(createdLullaby => {
+     return createdLullaby.json()
+   })
+  
+   .then(jsonedLullaby => {
+     // reset the form
+     // add notice to notices
+     this.setState({
+       formInputs: {
+         title: '',
+         artist: '',
+         lyrics: '',
+         image: '',
+         vid_link: ''
+        },
+       lullabies: [jsonedLullaby, ...this.state.lullabies]
+     })
+   })
+   .catch(error => console.log(error))
+  }
+  
+   componentDidMount() {
+    this.getNotices()
+  }
+  
+  getNotices = () =>{
+    fetch('http://localhost:3000/lullabies')
+      .then(response => response.json())
+      .then(json => this.setState({lullabies: json}))
+      .catch(error => console.error(error))
+  }
+  
+
   render() {
   return (
     <div className="App">
+      <div className="container">
       <Lullabies />
+      </div>
       
     </div>
   );
--- lullaby_client/src/components/Lullabies.js
+++ lullaby_client/src/components/Lullabies.js
@@ -3,7 +3,24 @@ import React, {Component} from 'react';
 class Lullabies extends Component {
 
     state = {
-        lullabies: []
+        lullabies: [],
+        formInputs: {
+          title: '',
+          artist: '',
+          lyrics: '',
+          image: '',
+          vid_link: ''
+          }
+        }
+
+    handleChange = (event) => {
+        const updateInput = Object.assign( this.state.formInputs, { [event.target.id]: event.target.value })
+        this.setState(updateInput)
+        }
+        
+    handleSubmit  = (event) =>{
+        event.preventDefault()
+        console.log(this.state.formInputs)
     }
 
     componentDidMount() {
@@ -13,12 +30,28 @@ class Lullabies extends Component {
     getLullabies = () =>{
         fetch('http://localhost:3000/lullabies')
             .then(response => response.json())
-            .then(json => console.log(json))
+            .then(json => this.setState({lullabies: json}))
         .catch(error => console.error(error))
     }
+    
     render() {
+        console.log(this.state.lullabies)
         return (
+        <div>
             <h1>Sweet Child of Mine</h1>
+        
+
+            { this.state.lullabies.map (lullaby => {
+                return(
+                    <div key = {lullaby.id} className = "card">
+                        <p>{lullaby.title}</p>
+                        <img src={lullaby.image}></img>
+                        <p>{lullaby.lyrics}</p>
+                        <p>{lullaby.artist}</p>
+                    </div>
+                )
+            })}
+        </div>
         )
     }
 }
